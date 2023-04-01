0fe588e52239aca2e256ffd8d291f6063ec8ee61
Nitya Ranjan <43794015+nityaranjan22@users.noreply.github.com>
2023-03-31 22:45:06 -0400

    PWM test to check what duty cycle is unbearable

--- pwm_check.py
+++ pwm_check.py
@@ -10,26 +10,28 @@ TIME_STEP = 0.01  # Time step in seconds for updating the duty cycle
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PWM_PIN, GPIO.OUT)
 
+
 # Create a PWM object with a 100 Hz frequency
 pwm = GPIO.PWM(PWM_PIN, PWM_FREQ)
-pwm.start(100)  # Start PWM with a 0% duty cycle
-
-try:
-    while True:
-        pwm.ChangeDutyCycle(100)
-        time.sleep(TIME_STEP)
+pwm.start(0)  # Start PWM with a 0% duty cycle
 
 # try:
 #     while True:
-#         # Gradually increase the duty cycle from 0 to 100%
-#         for duty_cycle in range(101):
-#             pwm.ChangeDutyCycle(duty_cycle)
-#             time.sleep(TIME_STEP)
+#         pwm.ChangeDutyCycle(100)
+#         time.sleep(TIME_STEP)
 
-#         # Gradually decrease the duty cycle from 100 to 0%
-#         for duty_cycle in range(100, -1, -1):
-#             pwm.ChangeDutyCycle(duty_cycle)
-#             time.sleep(TIME_STEP)
+try:
+    while True:
+        # Gradually increase the duty cycle from 0 to 100%
+        for duty_cycle in range(101):
+            pwm.ChangeDutyCycle(duty_cycle)
+            print(f'DUTY CYCLE:{duty_cycle}')
+            time.sleep(0.5)
+
+        # # Gradually decrease the duty cycle from 100 to 0%
+        # for duty_cycle in range(100, -1, -1):
+        #     pwm.ChangeDutyCycle(duty_cycle)
+        #     time.sleep(TIME_STEP)
 
 except KeyboardInterrupt:
     print("\nStopping PWM test.")
