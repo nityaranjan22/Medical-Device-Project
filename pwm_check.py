f60f5d7a6fed171ae6d27c212d5f36bbc8baf583
Nitya Ranjan <43794015+nityaranjan22@users.noreply.github.com>
2023-04-01 17:18:02 -0400

    edited pwm

--- pwm_check.py
+++ pwm_check.py
@@ -26,19 +26,26 @@ i2c = board.I2C()
 
 temp_sensor = adafruit_adt7410.ADT7410(i2c, address=0x49)
 
+# try:
+#     while True:
+#         # Gradually increase the duty cycle from 0 to 100%
+#         for duty_cycle in range(101):
+#             pwm.ChangeDutyCycle(duty_cycle)
+#             temp = temp_sensor.temperature
+#             print(f'DUTY CYCLE:{duty_cycle}     TEMPERATURE:{temp}')
+#             time.sleep(0.5)
+
+#         # # Gradually decrease the duty cycle from 100 to 0%
+#         # for duty_cycle in range(100, -1, -1):
+#         #     pwm.ChangeDutyCycle(duty_cycle)
+#         #     time.sleep(TIME_STEP)
+
 try:
     while True:
-        # Gradually increase the duty cycle from 0 to 100%
-        for duty_cycle in range(101):
-            pwm.ChangeDutyCycle(duty_cycle)
+            pwm.ChangeDutyCycle(100)
             temp = temp_sensor.temperature
-            print(f'DUTY CYCLE:{duty_cycle}     TEMPERATURE:{temp}')
+            print(f'DUTY CYCLE:{100}     TEMPERATURE:{temp}')
             time.sleep(0.5)
-
-        # # Gradually decrease the duty cycle from 100 to 0%
-        # for duty_cycle in range(100, -1, -1):
-        #     pwm.ChangeDutyCycle(duty_cycle)
-        #     time.sleep(TIME_STEP)
 
 except KeyboardInterrupt:
     print("\nStopping PWM test.")
